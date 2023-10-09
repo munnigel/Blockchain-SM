0482ff87bcc57e64f4cc67b6f7b3aa085c4295d1
Dayan <dayannbryan1@gmail.com>
2023-10-09 10:56:25 +0300

    Update Rightbar

--- client/src/components/rightBar/RightBar.tsx
+++ client/src/components/rightBar/RightBar.tsx
@@ -10,10 +10,10 @@ const RightBar = () => {
             <div className="rightBar__container__item__user__userInfo">
               <img 
                 className="rightBar__container__item__user__userInfo__userimage"
-                src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600" 
+                src="https://images.pexels.com/photos/4902634/pexels-photo-4902634.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" 
                 alt="" 
               />
-              <span className="rightBar__container__item__user__userInfo__username">Loren Doe</span>
+              <span className="rightBar__container__item__user__userInfo__username">Loren Nahshon</span>
             </div>
             <div className="rightBar__container__item__user__buttons">
               <button className="rightBar__container__item__user__buttons__followBtn">follow</button>
@@ -24,14 +24,136 @@ const RightBar = () => {
             <div className="rightBar__container__item__user__userInfo">
               <img 
                 className="rightBar__container__item__user__userInfo__userimage"
-                src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600" 
+                src="https://images.pexels.com/photos/4902634/pexels-photo-4902634.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" 
                 alt="" 
               />
-              <span className="rightBar__container__item__user__userInfo__username">Loren Doe</span>
+              <span className="rightBar__container__item__user__userInfo__username">Loren Nahshon</span>
             </div>
             <div className="rightBar__container__item__user__buttons">
               <button className="rightBar__container__item__user__buttons__followBtn">follow</button>
               <button className="rightBar__container__item__user__buttons__dismissBtn">dismiss</button>
+            </div>
+          </div>
+        </div>
+        <div className="rightBar__container__item">
+          <span className="rightBar__container__item__title">Latest Activities</span>
+          <div className="rightBar__container__item__user">
+            <div className="rightBar__container__item__user__userInfo">
+              <img 
+                className="rightBar__container__item__user__userInfo__userimage"
+                src="https://images.pexels.com/photos/4902634/pexels-photo-4902634.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" 
+                alt="" 
+              />
+              <p className="rightBar__container__item__user__userInfo__p">
+                <span className="rightBar__container__item__user__userInfo__username">Loren Nahshon</span> changed their cover picture
+              </p>
+            </div>
+            <span className="rightBar__container__item__user__timeActivity">1 min ago</span>
+          </div>
+          <div className="rightBar__container__item__user">
+            <div className="rightBar__container__item__user__userInfo">
+              <img 
+                className="rightBar__container__item__user__userInfo__userimage"
+                src="https://images.pexels.com/photos/4902634/pexels-photo-4902634.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" 
+                alt="" 
+              />
+              <p className="rightBar__container__item__user__userInfo__p">
+                <span className="rightBar__container__item__user__userInfo__username">Loren Nahshon</span> changed their cover picture
+              </p>
+            </div>
+            <span className="rightBar__container__item__user__timeActivity">1 min ago</span>
+          </div>
+          <div className="rightBar__container__item__user">
+            <div className="rightBar__container__item__user__userInfo">
+              <img 
+                className="rightBar__container__item__user__userInfo__userimage"
+                src="https://images.pexels.com/photos/4902634/pexels-photo-4902634.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" 
+                alt="" 
+              />
+              <p className="rightBar__container__item__user__userInfo__p">
+                <span className="rightBar__container__item__user__userInfo__username">Loren Nahshon</span> changed their cover picture
+              </p>
+            </div>
+            <span className="rightBar__container__item__user__timeActivity">1 min ago</span>
+          </div>
+        </div>
+        <div className="rightBar__container__item">
+          <span className="rightBar__container__item__title">Online Friends</span>
+          <div className="rightBar__container__item__user">
+            <div className="rightBar__container__item__user__userInfo">
+              <img 
+                className="rightBar__container__item__user__userInfo__userimage"
+                src="https://images.pexels.com/photos/4902634/pexels-photo-4902634.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" 
+                alt="" 
+              />
+              <div className="rightBar__container__item__user__userInfo__online"/>
+              <span className="rightBar__container__item__user__userInfo__username">Loren Nahshon</span>
+            </div>
+          </div>
+          <div className="rightBar__container__item__user">
+            <div className="rightBar__container__item__user__userInfo">
+              <img 
+                className="rightBar__container__item__user__userInfo__userimage"
+                src="https://images.pexels.com/photos/4902634/pexels-photo-4902634.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" 
+                alt="" 
+              />
+              <div className="rightBar__container__item__user__userInfo__online"/>
+              <span className="rightBar__container__item__user__userInfo__username">Loren Nahshon</span>
+            </div>
+          </div>
+          <div className="rightBar__container__item__user">
+            <div className="rightBar__container__item__user__userInfo">
+              <img 
+                className="rightBar__container__item__user__userInfo__userimage"
+                src="https://images.pexels.com/photos/4902634/pexels-photo-4902634.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" 
+                alt="" 
+              />
+              <div className="rightBar__container__item__user__userInfo__online"/>
+              <span className="rightBar__container__item__user__userInfo__username">Loren Nahshon</span>
+            </div>
+          </div>
+          <div className="rightBar__container__item__user">
+            <div className="rightBar__container__item__user__userInfo">
+              <img 
+                className="rightBar__container__item__user__userInfo__userimage"
+                src="https://images.pexels.com/photos/4902634/pexels-photo-4902634.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" 
+                alt="" 
+              />
+              <div className="rightBar__container__item__user__userInfo__online"/>
+              <span className="rightBar__container__item__user__userInfo__username">Loren Nahshon</span>
+            </div>
+          </div>
+          <div className="rightBar__container__item__user">
+            <div className="rightBar__container__item__user__userInfo">
+              <img 
+                className="rightBar__container__item__user__userInfo__userimage"
+                src="https://images.pexels.com/photos/4902634/pexels-photo-4902634.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" 
+                alt="" 
+              />
+              <div className="rightBar__container__item__user__userInfo__online"/>
+              <span className="rightBar__container__item__user__userInfo__username">Loren Nahshon</span>
+            </div>
+          </div>
+          <div className="rightBar__container__item__user">
+            <div className="rightBar__container__item__user__userInfo">
+              <img 
+                className="rightBar__container__item__user__userInfo__userimage"
+                src="https://images.pexels.com/photos/4902634/pexels-photo-4902634.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" 
+                alt="" 
+              />
+              <div className="rightBar__container__item__user__userInfo__online"/>
+              <span className="rightBar__container__item__user__userInfo__username">Loren Nahshon</span>
+            </div>
+          </div>
+          <div className="rightBar__container__item__user">
+            <div className="rightBar__container__item__user__userInfo">
+              <img 
+                className="rightBar__container__item__user__userInfo__userimage"
+                src="https://images.pexels.com/photos/4902634/pexels-photo-4902634.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" 
+                alt="" 
+              />
+              <div className="rightBar__container__item__user__userInfo__online"/>
+              <span className="rightBar__container__item__user__userInfo__username">Loren Nahshon</span>
             </div>
           </div>
         </div>

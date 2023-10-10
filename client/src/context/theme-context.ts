786ad061d793fb6df0e91d0df5222f38c66f990b
Dayan <dayannbryan1@gmail.com>
2023-10-10 11:12:16 +0300

    Update

--- client/src/context/theme-context.ts
+++ client/src/context/theme-context.ts
@@ -2,6 +2,7 @@ import { createContext } from "react"
 
 
 export const DarkModeContext = createContext({
-    darkMode: '',
-    setDarkMode: (darkMode: string) => {}
+    darkMode: 'light',
+    setDarkMode: () => {}
 })
+
--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -4,40 +4,7 @@ const Home = () => {
   return (
     <div className="home">
       <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
+      
     </div>
   )
 }

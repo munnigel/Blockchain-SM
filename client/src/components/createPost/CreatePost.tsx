f6af449567a997d9d27e65436d088ac6261d80c9
chuanshaof <chuanshaof@gmail.com>
2023-11-22 21:34:02 +0800

    integration with chain, needs updating of lib

--- client/src/components/createPost/CreatePost.tsx
+++ client/src/components/createPost/CreatePost.tsx
@@ -2,7 +2,8 @@ import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { addPost } from '../../store/postSlice';  // Adjust the import path to your postsSlice
 import './CreatePost.scss';
-import { UploadOutlined } from '@ant-design/icons';
+import CreatePostButton from './CreatePostButton';
+// import { UploadOutlined } from '@ant-design/icons';
 
 interface RootState {
   posts: {
@@ -20,38 +21,44 @@ const CreatePost: React.FC = () => {
     const posts = useSelector((state: RootState) => state.posts);
 
     const [postContent, setPostContent] = useState<string>('');
-    const [postImage, setPostImage] = useState<string>('');
+    // const [postImage, setPostImage] = useState<string>('');
 
     const handlePostChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
         setPostContent(event.target.value);
     };
 
-    const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        if (event.target.files && event.target.files[0]) {
-            // You can perform image validation here if needed
-            const imageFile = event.target.files[0];
-            const imageUrl = URL.createObjectURL(imageFile);
-            setPostImage(imageUrl);
-        }
-    };
+    // TODO: Post image is going to be work in progress on the smart contract
+    // const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    //     if (event.target.files && event.target.files[0]) {
+    //         // You can perform image validation here if needed
+    //         const imageFile = event.target.files[0];
+    //         const imageUrl = URL.createObjectURL(imageFile);
+    //         setPostImage(imageUrl);
+    //     }
+    // };
 
     const handleShare = () => {
         // In a real-world app, you'd also want to upload the image to a server
         // and get back a URL for the image, instead of directly using a local URL.
+        
 
+        // TODO: Modify all of these 
         const newPost = {
             id: posts.length + 1,
             name: "Anastasia Sh.",
             userId: 1,
             profilePic: "https://images.pexels.com/photos/5704849/pexels-photo-5704849.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
             description: postContent,
-            image: postImage,  // Here you set the image URL
+            image: "",
+            // image: postImage,  // Here you set the image URL
+            // FIXME: Might want to change the initial addPost
+            timestamp: Date.now().toString(), // Fix: Convert timestamp to string
         };
 
         dispatch(addPost(newPost));
 
         setPostContent('');
-        setPostImage('');
+        // setPostImage('');
     };
 
     return (
@@ -61,23 +68,25 @@ const CreatePost: React.FC = () => {
                 value={postContent}
                 onChange={handlePostChange}
             />
-            <input
+            {/* TODO: Post image is going to be work in progress on the smart contract */}
+            {/* <input
                 type="file"
                 accept="image/*"
                 onChange={handleImageChange}
                 style={{ display: 'none' }}  // Hide the default file input
                 id="fileInput"
-            />
-            {postImage && (
+            /> */}
+            {/* {postImage && (
                 <div className="image-preview">
                     <img src={postImage} alt="Preview" />
                 </div>
-            )}
-            <label htmlFor="fileInput" className="upload-image-button">
+            )} */}
+            {/* <label htmlFor="fileInput" className="upload-image-button">
                 <UploadOutlined />
-            </label>
+            </label> */}
             
-            <button onClick={handleShare}>Share</button>
+            {/* <button onClick={handleShare}>Share</button> */}
+            <CreatePostButton/>
         </div>
     );
 }
